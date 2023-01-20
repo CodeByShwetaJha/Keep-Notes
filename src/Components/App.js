d2219e02dc1d15b58c21644f8b3f05a5b73573cd
Shweta kumari <46412339+CodeByShwetaJha@users.noreply.github.com>
2023-01-20 21:59:42 +0530

    Files updated

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import Header from "./component/Header";
-import Footer from "./component/Footer";
-import Note from "./component/Note";
-import CreateArea from "./component/CreateArea";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
 
 function App() {
   const [notes, setNotes] = useState([]);
